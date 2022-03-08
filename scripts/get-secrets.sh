60795001ced215b22ec2780a445588926a527473
BasharKhouri-Voice <bkhoury@voice.com>
2022-03-08 04:10:46 +0300

    fix script

--- scripts/get-secrets.sh
+++ scripts/get-secrets.sh
@@ -6,7 +6,6 @@ jq --arg elastic_user $ELASTIC_USER --arg  elastic_pass $ELASTIC_PASS --arg elas
     '.elasticsearch.user = $elastic_user | .elasticsearch.pass =  $elastic_pass | .elasticsearch.host = $elastic_host | .elasticsearch.ingest_nodes = [$elastic_host] '  \
     /tmp/connections.json > /hyperion-history-api/connections.json
 
-jq --arg  server_name $SERVER_NAME \
-    '.api.server_name = $server_name'  /tmp/voice.config.json> /hyperion-history-api/chains/voice.config.json
-
+jq --arg  server_name $SERVER_NAME  --arg  pod_ip $POD_IP \
+    '.api.server_name = $server_name |.api.server_addr = $POD_IP'  /tmp/voice.config.json> /hyperion-history-api/chains/voice.config.json
 ./run.sh $@
