90796e075197b5fcc4ba1d19de2dcffb7be01800
BasharKhouri-Voice <bkhoury@voice.com>
2022-03-08 17:58:23 +0300

    fixing empty str

--- scripts/get-secrets.sh
+++ scripts/get-secrets.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 set -eu
 
-jq --arg elastic_user $ELASTIC_USER --arg  elastic_pass $ELASTIC_PASS --arg elastic_host $ELASTIC_HOST \
+jq -n --arg  elastic_user $ELASTIC_USER --arg  elastic_pass $ELASTIC_PASS --arg elastic_host $ELASTIC_HOST \
     '.elasticsearch.user = $elastic_user | .elasticsearch.pass =  $elastic_pass | .elasticsearch.host = $elastic_host | .elasticsearch.ingest_nodes = [$elastic_host] '  \
     /tmp/connections.json > /hyperion-history-api/connections.json
 
