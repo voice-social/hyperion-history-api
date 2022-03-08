af8e324ca01c0d7fcb251ffe896a48d5d357f323
BasharKhouri-Voice <bkhoury@voice.com>
2022-03-08 18:40:49 +0300

    added extra vars

--- scripts/get-secrets.sh
+++ scripts/get-secrets.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -eu
-export NO_PROXY="$NO_PROXY:$POD_IP"
-
 jq -n --arg  elastic_user $ELASTIC_USER  --arg  elastic_pass $ELASTIC_PASS --arg elastic_host $ELASTIC_HOST \
     '.elasticsearch.user = $elastic_user | .elasticsearch.pass =  $elastic_pass | .elasticsearch.host = $elastic_host | .elasticsearch.ingest_nodes = [$elastic_host] '  \
     /tmp/connections.json > /hyperion-history-api/connections.json
 
-jq -n --arg server_name $SERVER_NAME --arg pod_ip "$POD_IP" \
+jq -n --arg server_name $SERVER_NAME \
     '.api.server_name = $server_name | .api.server_addr = $pod_ip'  /tmp/voice.config.json > /hyperion-history-api/chains/voice.config.json
 ./run.sh $@
