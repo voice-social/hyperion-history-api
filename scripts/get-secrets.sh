b28ea0cf0891fea86db58cc609d6e30ab44475af
BasharKhouri-Voice <bkhoury@voice.com>
2022-03-10 21:18:18 +0300

    fix the get_secret script

--- scripts/get-secrets.sh
+++ scripts/get-secrets.sh
@@ -5,5 +5,5 @@ jq -n --arg  elastic_user $ELASTIC_USER  --arg  elastic_pass $ELASTIC_PASS --arg
     /tmp/connections.json > /hyperion-history-api/connections.json
 
 jq -n --arg server_name $SERVER_NAME \
-    '.api.server_name = $server_name | .api.server_addr = $pod_ip'  /tmp/voice.config.json > /hyperion-history-api/chains/voice.config.json
+    '.api.server_name = $server_name'  /tmp/voice.config.json > /hyperion-history-api/chains/voice.config.json
 ./run.sh $@
